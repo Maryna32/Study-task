ed348fa70749fa13ce800b472d6c758fdff7cc2d
Maryna32 <marinaburda053@gmail.com>
2025-10-03 19:41:59 +0300

    feat: show statistics for authenticated user

--- study-task/components/QuickStatistic.tsx
+++ study-task/components/QuickStatistic.tsx
@@ -1,5 +1,7 @@
+const info = null;
+
 function QuickStatistic() {
-  return <div>QuickStatistic</div>;
+  return <div>{info || <p>Для статистики вввійдіть в систему</p>}</div>;
 }
 
 export default QuickStatistic;
