dd75664511a1de7d81a86d36ff7263cb6e7c2a35
Maryna32 <marinaburda053@gmail.com>
2025-10-08 17:37:55 +0300

    add button to redirect on add-task page

--- study-task/app/tasks/page.tsx
+++ study-task/app/tasks/page.tsx
@@ -1,13 +1,22 @@
 "use client";
 import { Header, Filters, TaskList } from "@/components/index";
+import { useRouter } from "next/navigation";
+import { Button } from "../../components/ui/button";
 
 function TasksPage() {
+  const router = useRouter();
+  const goToAddTasks = () => {
+    router.push("/add-task");
+  };
   return (
     <div>
       <Header />
       <div className="pl-[30px] pt-[20px]">
         <Filters />
         <TaskList />
+        <Button size="sm" className="w-fit mt-2" onClick={goToAddTasks}>
+          Додати завдання
+        </Button>
       </div>
     </div>
   );
